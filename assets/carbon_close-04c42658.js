7088e95bfe4d05f74808dfb1f977abf85a893e16
Pavel Novikov <novikov@jobfrontend.ru>
2024-07-30 13:52:23 +0300

    Updates

---
+++ assets/carbon_close-04c42658.js
@@ -0,0 +1,1 @@
+import{o as e,c as t,b as o}from"./index-e1d1c71b.js";const n={xmlns:"http://www.w3.org/2000/svg",width:"24",height:"24",fill:"none"},s=o("path",{fill:"#fff",d:"M18 7.05 16.95 6 12 10.95 7.05 6 6 7.05 10.95 12 6 16.95 7.05 18 12 13.05 16.95 18 18 16.95 13.05 12z"},null,-1),c=[s];function r(l,a){return e(),t("svg",n,c)}const h={render:r};export{h as default,r as render};
